595d4197a25f70e42df5bf1852e96033a8246b3a
Farbod Nazari <81690217+farbodnm@users.noreply.github.com>
2024-02-02 17:25:12 +0330

    Add dispatch service module

---
+++ portal/services/dispatch/src/main/java/com/taxi/dispatch/DispatchApplication.java
@@ -0,0 +1,13 @@
+package com.taxi.dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchApplication.class, args);
+    }
+
+}
